3baeda185cd441489c3d606d7454756f3550a58b
Karel Zak <kzak@redhat.com>
2020-09-17 17:16:55 +0200

    mountbomber: add workers
    Signed-off-by: Karel Zak <kzak@redhat.com>

--- misc-utils/mountbomber.c
+++ misc-utils/mountbomber.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <stdarg.h>
 #include <libmount.h>
 #include <ctype.h>
@@ -73,9 +74,17 @@ struct bomber_cmd {
 	void *data;	/* private command data */
 };
 
+struct bomber_worker {
+	pid_t pid;
+	int status;		/* status as returned by wait() */
+	size_t pool_off;	/* first mountpoint */
+	size_t pool_len;	/* number of mounpoints assigned to the worker */
+
+	struct timeval starttime;
+};
+
 struct bomber_ctl {
 	size_t	nmounts;	/* --pool <size> */
-	size_t	nparallels;	/* --parallel <number> */
 
 	unsigned int freq;	/* number of operations per second */
 
@@ -83,6 +92,10 @@ struct bomber_ctl {
 
 	struct bomber_cmd *commands;
 	size_t ncommands;
+
+	struct bomber_worker *workers;
+	size_t nworkers;
+	size_t nactive;
 
 	unsigned int clean_dir : 1,
 		     carriage_ret: 1,
@@ -208,7 +221,7 @@ static int bomber_init_mountdir(struct bomber_ctl *ctl)
 
 		get_mountpoint_name(i, name, sizeof(name));
 
-		mesg_bar(ctl, _("initialize mountpoint: %05zu"), i);
+		mesg_bar(ctl, _("initialize mountpoint: %05zu"), i + 1);
 
 		rc = mkdir(name, mode);
 		if (rc && errno != EEXIST) {
@@ -238,7 +251,7 @@ static void bomber_cleanup_dir(struct bomber_ctl *ctl)
 
 		get_mountpoint_name(i, name, sizeof(name));
 
-		mesg_bar(ctl, _("cleanup mountpoint: %05zu"), i);
+		mesg_bar(ctl, _("cleanup mountpoint: %05zu"), i + 1);
 
 		if (rmdir(name) && errno != ENOENT)
 			mesg_warn(ctl, _("connot remove directory %s"), name);
@@ -250,9 +263,113 @@ static void bomber_cleanup_dir(struct bomber_ctl *ctl)
 		mesg_warn(ctl, _("connot remove directory %s"), ctl->dir);
 }
 
+static pid_t start_worker(struct bomber_ctl *ctl, struct bomber_worker *wrk)
+{
+	pid_t pid;
+	int status = 0;
+
+	switch ((pid = fork())) {
+	case -1:
+		warn(_("fork failed"));
+		return -errno;
+	case 0: /* child */
+		break;
+	default: /* parent */
+		return pid;
+	}
+
+	/* child main loop */
+	while (sig_die == 0) {
+		sleep(10);
+	};
+
+	exit(status);
+}
+
 static int bomber_init_pool(struct bomber_ctl *ctl)
 {
+	size_t i, off = 0, len;
+
 	assert(ctl);
+
+	len = ctl->nmounts / ctl->nworkers;
+
+	ctl->workers = xcalloc(ctl->nworkers, sizeof(struct bomber_worker));
+
+	for (i = 0; i < ctl->nworkers; i++) {
+		struct bomber_worker *w = &ctl->workers[i];
+
+		w->pool_off = off;
+		w->pool_len = len;
+		w->pid = start_worker(ctl, w);
+		if (w->pid <= 0)
+			return w->pid;
+		off += len;
+		ctl->nactive++;
+
+		mesg_bar(ctl, _("starting worker: %04zu"), i + 1);
+	}
+
+	mesg_bar_done(ctl);
+	return 0;
+}
+
+static void unlink_child(struct bomber_ctl *ctl, pid_t pid, int status)
+{
+	size_t i;
+
+	for (i = 0; i < ctl->nworkers; i++) {
+		struct bomber_worker *w = &ctl->workers[i];
+
+		if (w->pid == pid) {
+			w->pid = 0;
+			w->status = status;
+			ctl->nactive--;
+			break;
+		}
+	}
+}
+
+static int bomber_wait_pool(struct bomber_ctl *ctl, int flags)
+{
+	while (ctl->nactive > 0) {
+		int status = 0;
+		pid_t pid;
+
+		mesg_bar(ctl, _("active workers ... %04zu (waiting)"), ctl->nactive);
+
+		pid = waitpid(-1, &status, flags);
+		if (pid == 0 && (flags & WNOHANG))
+			return 0;
+		if (pid < 0) {
+			if (errno == EINTR || errno == EAGAIN)
+				continue;
+			if (errno == ECHILD)
+				break;
+			mesg_warn(ctl, _("waitpid failed"));
+			continue;
+		}
+		if (WIFEXITED(status) || WIFSIGNALED(status))
+			unlink_child(ctl, pid, status);
+	}
+
+	mesg_bar(ctl, _("active workers ... %04zu"), ctl->nactive);
+	mesg_bar_done(ctl);
+
+	return 1;	/* no more childs */
+}
+
+static int bomber_cleanup_pool(struct bomber_ctl *ctl)
+{
+	size_t i;
+
+	for (i = 0; i < ctl->nworkers; i++) {
+		struct bomber_worker *w = &ctl->workers[i];
+		if (w->pid > 0)
+			kill(w->pid, SIGTERM);
+	}
+
+	bomber_wait_pool(ctl, 0);
 
 	return 0;
 }
@@ -269,8 +386,6 @@ static inline size_t name2idx(const char *name, const char **ary, size_t arysz, 
 	errx(EXIT_FAILURE, errmsg, name);
 	return 0;
 }
-
-
 
 static int bomber_add_command(struct bomber_ctl *ctl, const char *str)
 {
@@ -366,7 +481,7 @@ int main(int argc, char *argv[])
 			ctl->nmounts = strtou32_or_err(optarg, _("failed to parse pool argument"));
 			break;
 		case 'x':
-			ctl->nparallels = strtou32_or_err(optarg, _("failed to parse parallel argument"));
+			ctl->nworkers = strtou32_or_err(optarg, _("failed to parse parallel argument"));
 			break;
 		case 'f':
 			ctl->freq = strtou32_or_err(optarg, _("failed to parse freq argument"));
@@ -386,8 +501,8 @@ int main(int argc, char *argv[])
 		errx(EXIT_FAILURE, _("pool size cannot be zero"));
 	if (!ctl->dir)
 		ctl->dir = xstrdup("/mnt/bomber");
-	if (!ctl->nparallels)
-		ctl->nparallels = ctl->nmounts / 10;
+	if (!ctl->nworkers)
+		ctl->nworkers = ctl->nmounts / 10;
 
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
@@ -397,11 +512,13 @@ int main(int argc, char *argv[])
 
 	rc = bomber_init_mountdir(ctl);
 	if (rc == 0)
-		bomber_init_pool(ctl);
-
+		rc = bomber_init_pool(ctl);
+	if (rc == 0)
+		bomber_wait_pool(ctl, 0);
 	if (sig_die)
 		mesg_warnx(ctl, _("interrupted by signal"));
 
+	bomber_cleanup_pool(ctl);
 	bomber_cleanup_dir(ctl);
 
 	return EXIT_SUCCESS;
